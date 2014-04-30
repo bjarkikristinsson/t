edb20e08e52032d8d5c676d94e08a8d0274a4c98
unknown <BjarkiH@TFW85959.skyrr.local>
2014-04-30 14:09:55 +0000

    breytti doti

---
+++ Skjatexti/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skjatexti.MvcApplication" Language="C#" %>
---
+++ Skjatexti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Skjatexti.Startup))]
+namespace Skjatexti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
